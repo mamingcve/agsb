188b74866995ce0a35881e094c377d8f2f5210dc
mamingcve <mamingcve@gmail.com>
2025-06-18 04:02:04 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,7 +31,7 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "qiuweiyoue"         # 用户名
+USER_NAME = "mamingcve"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 12250                   # Vmess端口，留空或0则自动生成
 DOMAIN = "zy.1004837.dpdns.org"                   # 域名，留空则自动获取
